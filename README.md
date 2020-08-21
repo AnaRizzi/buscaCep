8b88a16e06e4e484a909e469cb8eb8dd64f33222
Ana Rizzi <ana.rizzi.eu@gmail.com>
2020-08-21 16:13:41 -0300

    página que consome api dos correios e busca cep

---
+++ README.md
@@ -0,0 +1,4 @@
+Sistema de consulta de CEP
+
+Através do Web Service dos correios, a página busca o cep digitado e informa o endereço completo.
+Desenvolvido em C# e Asp.Net MVC
---
+++ consultaCep/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using consultaCep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace consultaCep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Endereco endereco)
+        {
+            var ws = new correio.AtendeClienteClient();
+
+            ModelState.Remove("Rua");
+            ModelState.Remove("Bairro");
+            ModelState.Remove("Cidade");
+            ModelState.Remove("Estado");
+
+            try
+            {
+                var enderecoCompleto = ws.consultaCEP(endereco.Cep);
+
+                endereco.Rua = enderecoCompleto.end;
+                endereco.Bairro = enderecoCompleto.bairro;
+                endereco.Cidade = enderecoCompleto.cidade;
+                endereco.Estado = enderecoCompleto.uf;
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("cep", "Cep não encontrado");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View(endereco);
+            }
+            else
+            {
+                endereco.Rua = "";
+                endereco.Bairro = "";
+                endereco.Cidade = "";
+                endereco.Estado = "";
+
+                return View(endereco);
+            }
+        }
+
+
+    }
+}
